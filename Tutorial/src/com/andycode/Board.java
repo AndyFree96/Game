a20c50d34a0663374dd7e22736420a7856995865
AndyFree96 <leoscotthg@gmail.com>
2019-10-11 16:46:10 +0800

    Moving sprite

--- Tutorial/src/com/andycode/Board.java
+++ Tutorial/src/com/andycode/Board.java
@@ -2,92 +2,69 @@ package com.andycode;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
-public class Board extends JPanel
-        implements Runnable {
-
-    private final int B_WIDTH = 350;
-    private final int B_HEIGHT = 350;
-    private final int INITIAL_X = -40;
-    private final int INITIAL_Y = -40;
-    private final int DELAY = 25;
-
-    private Image star;
-    private Thread animator;
-    private int x, y;
+public class Board extends JPanel implements ActionListener {
+    private Timer timer;
+    private SpaceShip spaceShip;
+    private final int DELAY = 10;
 
     public Board() {
         initBoard();
     }
 
     private void initBoard() {
+        addKeyListener(new TAdapter());
         setBackground(Color.BLACK);
-        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-        loadImage();
+        setFocusable(true);
 
-        x = INITIAL_X;
-        y = INITIAL_Y;
+        spaceShip = new SpaceShip();
 
-    }
-
-    private void loadImage() {
-        ImageIcon imageIcon = new ImageIcon("src/resources/star.png");
-        star = imageIcon.getImage();
-    }
-
-    @Override
-    public void addNotify() {
-        super.addNotify();
-
-        animator = new Thread(this);
-        animator.start();
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawStar(g);
+        doDrawing(g);
     }
 
-    private void drawStar(Graphics graphics) {
-        graphics.drawImage(star, x, y, this);
-        Toolkit.getDefaultToolkit().sync();
-    }
+    private void doDrawing(Graphics graphics) {
+        Graphics2D graphics2D = (Graphics2D) graphics;
 
-    private void cycle() {
-        x += 1;
-        y += 1;
+        graphics2D.drawImage(spaceShip.getImage(), spaceShip.getX(),
+                spaceShip.getY(), this);
 
-        if (y > B_HEIGHT) {
-            x = INITIAL_X;
-            y = INITIAL_Y;
-        }
     }
 
     @Override
-    public void run() {
-        long beforeTime, timeDiff, sleep;
-        beforeTime = System.currentTimeMillis();
-
-        while (true) {
-            cycle();
-            repaint();
-
-            timeDiff = System.currentTimeMillis() - beforeTime;
-            sleep = DELAY - timeDiff;
-
-            if (sleep < 0) {
-                sleep = 2;
-            }
-
-            try {
-                Thread.sleep(sleep);
-            } catch (InterruptedException ex) {
-                String msg = String.format("Thread interrupted: %s", ex.getMessage());
-                JOptionPane.showMessageDialog(this, msg, "Error", JOptionPane.ERROR_MESSAGE);
-            }
-
-            beforeTime = System.currentTimeMillis();
-        }
+    public void actionPerformed(ActionEvent e) {
+        step();
     }
+
+    private void step() {
+        spaceShip.move();
+
+        repaint(spaceShip.getX() - 1, spaceShip.getY() - 1,
+                spaceShip.getWidth() + 2, spaceShip.getHeight() + 2);
+    }
+
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyReleased(KeyEvent e) {
+            spaceShip.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            spaceShip.keyPressed(e);
+        }
+
+    }
+
 }
