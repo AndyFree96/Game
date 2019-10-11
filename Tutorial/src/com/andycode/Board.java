587bd638d9a92148bf519aad62bdd3438368836d
AndyFree96 <leoscotthg@gmail.com>
2019-10-11 14:59:05 +0800

    Drawing an image

--- Tutorial/src/com/andycode/Board.java
+++ Tutorial/src/com/andycode/Board.java
@@ -1,37 +1,31 @@
 package com.andycode;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
 
 public class Board extends JPanel {
+    private Image image;
+
+    public Board() {
+       initBoard();
+    }
+
+    private void initBoard() {
+        loadImage();
+
+        int w = image.getWidth(this);
+        int h = image.getHeight(this);
+        setPreferredSize(new Dimension(w, h));
+    }
+
+    private void loadImage() {
+        ImageIcon imageIcon = new ImageIcon("src/resources/demo.png");
+        image = imageIcon.getImage();
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        drawDonut(g);
+        g.drawImage(image, 0, 0, null);
     }
 
-    private void drawDonut(Graphics graphics) {
-        Graphics2D graphics2D = (Graphics2D) graphics;
-        RenderingHints renderingHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        renderingHints.put(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-        graphics2D.setRenderingHints(renderingHints);
-        Dimension size = getSize();
-        double w = size.getWidth();
-        double h = size.getHeight();
-
-        Ellipse2D ellipse2D = new Ellipse2D.Double(0, 0, 80, 130);
-        graphics2D.setStroke(new BasicStroke(1));
-        graphics2D.setColor(Color.gray);
-
-        for (double deg = 0; deg < 360; deg += 5) {
-            AffineTransform affineTransform =
-                    AffineTransform.getTranslateInstance(w / 2, h / 2);
-            affineTransform.rotate(Math.toRadians(deg));
-            graphics2D.draw(affineTransform.createTransformedShape(ellipse2D));
-        }
-    }
 }
---
+++ Tutorial/src/com/andycode/ImageExample.java
@@ -0,0 +1,27 @@
+package com.andycode;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageExample extends JFrame {
+    public ImageExample() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        pack();
+
+        setTitle("Imgae");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()->{
+            ImageExample ex = new ImageExample();
+            ex.setVisible(true);
+        });
+    }
+}
