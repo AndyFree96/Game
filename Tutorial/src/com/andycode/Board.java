fd059b225ac7ae1198ba923c0d0ac6b512b1ea20
AndyFree96 <leoscotthg@gmail.com>
2019-10-11 15:54:23 +0800

    Swing timer

--- Tutorial/src/com/andycode/Board.java
+++ Tutorial/src/com/andycode/Board.java
@@ -2,30 +2,62 @@ package com.andycode;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Board extends JPanel {
-    private Image image;
+public class Board extends JPanel implements ActionListener {
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int INITIAL_X = -40;
+    private final int INITIAL_Y = -40;
+    private final int DELAY = 25;
+
+    private Image star;
+    private Timer timer;
+    private int x, y;
 
     public Board() {
-       initBoard();
+        initBoard();
     }
 
     private void initBoard() {
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
         loadImage();
 
-        int w = image.getWidth(this);
-        int h = image.getHeight(this);
-        setPreferredSize(new Dimension(w, h));
+        x = INITIAL_X;
+        y = INITIAL_Y;
+
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
 
     private void loadImage() {
-        ImageIcon imageIcon = new ImageIcon("src/resources/demo.png");
-        image = imageIcon.getImage();
+        ImageIcon imageIcon = new ImageIcon("src/resources/star.png");
+        star = imageIcon.getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.drawImage(image, 0, 0, null);
+        super.paintComponent(g);
+        drawStar(g);
     }
 
+    private void drawStar(Graphics graphics) {
+        graphics.drawImage(star, x, y, this);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        x += 1;
+        y += 1;
+
+        if (y > B_HEIGHT) {
+            x = INITIAL_X;
+            y = INITIAL_Y;
+        }
+
+        repaint();
+    }
 }
