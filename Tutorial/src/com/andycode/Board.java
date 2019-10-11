19170296a635ae1d4b600a15aa0b9df28bb91791
AndyFree96 <leoscotthg@gmail.com>
2019-10-11 14:34:38 +0800

    Application

---
+++ Tutorial/src/com/andycode/Board.java
@@ -0,0 +1,12 @@
+package com.andycode;
+
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+
+public class Board extends JPanel {
+    public Board() {
+
+    }
+}
