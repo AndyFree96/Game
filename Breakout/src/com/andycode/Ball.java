c356ff96f2db13709b89bf42df044b7954f16758
AndyFree96 <leoscotthg@gmail.com>
2019-10-14 14:09:56 +0800

    Breakout

---
+++ Breakout/src/com/andycode/Ball.java
@@ -0,0 +1,61 @@
+package com.andycode;
+
+import javax.swing.ImageIcon;
+
+public class Ball extends Sprite {
+    private int xdir;
+    private int ydir;
+
+    public Ball() {
+        initBall();
+    }
+
+    private void initBall() {
+        xdir = 1;
+        ydir = -1;
+
+        loadImage();
+        getImageDimensions();
+        resetState();
+    }
+
+    private void loadImage() {
+        ImageIcon ii = new ImageIcon("src/resources/ball.png");
+        image = ii.getImage();
+    }
+
+    void move() {
+        x += xdir;
+        y += ydir;
+
+        if (x == 0) {
+            setXDir(1);
+        }
+
+        if (x == Commons.WIDTH - imageWidth) {
+            setXDir(-1);
+        }
+
+        if (y == 0) {
+            setYDir(1);
+        }
+    }
+
+    private void resetState() {
+        x = Commons.INIT_BALL_X;
+        y = Commons.INIT_BALL_Y;
+    }
+
+    void setXDir(int xDir) {
+        this.xdir = xDir;
+    }
+
+    void setYDir(int yDir) {
+        this.ydir = yDir;
+    }
+
+    int getYdir() {
+        return ydir;
+    }
+
+}
